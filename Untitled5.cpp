304f3808579faecd747639f4814c0a82f03e05dc
unknown <s10601053@ym.edu.tw>
2021-09-26 19:45:44 +0800

    b1等

---
+++ Untitled5.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main(){
+    srand(time(NULL));
+    int i;
+    for(i=1; i<70; i++){
+        char fileName[20];
+        sprintf(fileName, "b%d.txt", i);
+        printf("fsutil file createnew %s %d\n", fileName, 100000000+i);
+        FILE* A = fopen(fileName, "w");
+        int j;
+        for(j=0; j<100000000+i; j++){
+            char r = rand(); // 亂碼之神大顯神功~
+            fprintf(A, "%c", r);
+        }
+        system("pause");
+        fclose(A);
+    }
+}
