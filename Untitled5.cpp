0f4efa1049f1eba604ffe2fa620c5bd9e3141c9f
unknown <s10601053@ym.edu.tw>
2021-09-27 13:47:17 +0800

    b70~89

--- Untitled5.cpp
+++ Untitled5.cpp
@@ -1,3 +1,6 @@
+/* Warning: Algorithm not optimized by coding; please use -O3 compiler.
+Time complexity: O(N) */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,7 +9,7 @@
 int main(){
     srand(time(NULL));
     int i;
-    for(i=4; i<70; i++){
+    for(i=70; i<90; i++){
         char fileName[20];
         sprintf(fileName, "b%d.txt", i);
         printf("fsutil file createnew %s %d\n", fileName, 100000000+i);
@@ -14,7 +17,7 @@ int main(){
         int j;
         for(j=0; j<100000000+i; j++){
             char r = rand(); // 亂碼之神大顯神功~
-            fprintf(A, "%c", r);
+            fprintf(A, "%c", r); // Rate determining step
         }
         fclose(A);
     }
